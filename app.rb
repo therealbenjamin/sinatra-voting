44025ea47296dafde95324c234990a46fe2edda8
Ben Blackburn <benjamin.blackburn@gmail.com>
2017-07-20 13:05:00 -0500

    add vote casting post endpoint

--- app.rb
+++ app.rb
@@ -8,5 +8,14 @@ Choices = {
 }
 
 get '/' do
+  @title = 'Vote for Sinatra!'
   erb :index
 end
+
+post '/cast' do
+  @title = 'Thank you, come again.'
+  @vote = params['vote']
+  erb :cast
+end
+
+
